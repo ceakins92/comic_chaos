bbac5f3caf6476aadb3ebe0a4186853f16a293f7
Chris Eakins <ceakins92@gmail.com>
2023-06-07 18:08:48 -0600

    Fixes

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
-from requests import get
+import requests
 import hashlib
+import pendulum
 import os
 import urllib.request, json
 from . import bp
@@ -8,71 +9,74 @@ from app.models import Post, User
 from app.blueprints.api.helpers import token_required
 
 
+# - ROUTE FOR char_page
 
 @bp.route('/char_page')
 def char_page():
     return render_template('char_page.jinja')
 
-@bp.route('/char_page', methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        char_name = request.form["search_char"]
-        Marvel_Char(char_name)
+# - Character search API
+
+def get_marvel_character(name):
+    print(name)
+    # - Get Public Key from env
+    public_key = os.environ.get('PUBLIC_KEY')
+    # - Get Timestamp for Marvel API requirement
+    ts = pendulum.now('UTC')
+    ts = ts.to_iso8601_string()
+    # - Generate Hash for Marvel API requirement
+    hash = hashlib.md5()
+    hash.update(ts.encode('utf-8'))
+    hash.update(os.environ.get('PRIVATE_KEY').encode('utf-8'))
+    hash.update(os.environ.get('PUBLIC_KEY').encode('utf-8'))
+    # - Set required Params
+    params = {
+        'apikey': public_key,
+        'ts': ts,
+        'hash': hash.hexdigest(),
+        'name': name
+    }
+    # - API call
+    response = requests.get(
+        'http://gateway.marvel.com/v1/public/characters', params=params)
+    # - Check Response code and generate data from json response
+    if response.status_code == 200:
+        print(response.json())
+        data = response.json()
+        # - Format data to retrieve desired output
+        try:
+            char_name = data["data"]["results"][0]["name"]
+            description = data["data"]["results"][0]["description"]
+            comics_avail = data["data"]["results"][0]["comics"]["available"]
+            comic_resource = data["data"]["results"][0]["comics"]["collectionURI"]
+            thumbnail = data["data"]["results"][0]["thumbnail"]["path"]
+            extension = data["data"]["results"][0]["thumbnail"]["extension"]
+            thumbnail = f"{thumbnail}/landscape_incredible.{extension}"
+            return {"Character Name": char_name, "Description (if available)": description, "Comics Appeared In": comics_avail, "Comic Collection via Marvel": comic_resource,
+                  "thumbnail": thumbnail}
+        # - Error handling for output
+        except:
+            print('Error retrieving Character.  Please try again.')
+    # - Error handling for API Response
     else:
-        return render_template("/char_page")
+        print('Error, please try again')
 
-class Marvel_Char():
-    def __init__(self,char_name):
-        self.name = char_name
-        self.image = None
-        self.call_marvel_api()
-
-    def call_marvel_api(self):
-        if isinstance(self.name, str) and self.name.isalpha():
-            self.name = self.name.lower()
-        response = get(f'https://metron.cloud/api/character/?name={self.name}')
-        if response.status_code == 200:
-            print('Success')
-            data = response.json()
-            self.name = data.results[0]["name"]
-            self.image = data.results[0]["cover"]
-            if not self.image:
-                self.image = None
-        else:
-            print(f'Error status code {response.status_code}')
-        return self.name
-
-## def get_character(PUBLIC_KEY,PRIVATE_KEY,name):
-##    now=pendulum.now('UTC')
-##    now=now.to_iso8601_string()
-##
-##   m=hashlib.md5()
-##   m.update(now.encode('utf8'))
-##   m.update(os.environ.get({PRIVATE_KEY}).encode('utf8'))
-##   m.update(os.environ.get({PUBLIC_KEY}).encode('utf8'))
-##
-##   endpoint = f'https://gateway.marvel.com:443/v1/public/characters?nameStartsWith={name}&limit=1'
-##   resp = requests.get(endpoint, params={"apikey": PUBLIC_KEY, "ts": now, "hash": m.hexdigest()}).json()
-##
-##   try:
-##      name = resp["data"]["results"][0]["name"]
-##      description = resp["data"]["results"][0]["description"]
-##      thumbnail = resp["data"]["results"][0]["thumbnail"]["path"]
-##      extension = resp["data"]["results"][0]["thumbnail"]["extension"]
-##      thumbnail = f"{thumbnail}/landscape_incredible.{extension}"
-##      print({"name": name, "description": description, "thumbnail": thumbnail})
-##      return {"name": name, "description": description, "thumbnail": thumbnail}
-##   except IndexError:
-##      return render_template("/char_page")
    
-##@bp.route('/char_page', methods=["GET", "POST"])
-##def index():
-##    if request.method == "POST":
-##        name = request.form["search"]
-##        data = get_character(os.environ.get('PUBLIC_KEY'),os.environ.get('PRIVATE_KEY'), name)
-##        return render_template("/char_page", data=data)
-    
-##    return render_template("/char_page")
+@bp.route('/char_page', methods=['GET','POST'])
+def char_page_post():
+    if request.method == 'POST':
+        try:
+            name = request.form.get['search_char']
+            data = get_marvel_character(name)
+            if data:
+                return render_template('char_page.jinja', data=data)
+            else:
+                return "Error, Character not found."
+        except:
+            return "Error, something went wrong, pleas try again."
+    else:
+        return render_template('char_page.jinja')
+
 
 
 def on_sale():
--- test.py
+++ test.py
@@ -1,8 +1,39 @@
+import hashlib
+from pendulum import time
 import requests
 
-def getit(name):
-    resp = requests.get(f'https://metron.cloud/api/character/?name={name}')
-    print(resp)
+def get_marvel_character(name):
+    public_key = '105d96a57df71042ffdfc572b7195cf5'
+    private_key = 'e917c38970ddb27117e675af6518098d573d06c4'
+    ts = str(time.time())
+    hash = hashlib.md5()
+    hash.update(ts.encode('utf-8'))
+    hash.update(private_key.encode('utf-8'))
+    hash.update(public_key.encode('utf-8'))
+    params = {
+        'apikey': public_key,
+        'ts': ts,
+        'hash': hash.hexdigest(),
+        'name': name
+    }
+    response = requests.get('http://gateway.marvel.com/v1/public/characters', params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+    
+new_search = get_marvel_character()
+print(new_search(deadpool))
 
 
-getit(deadpool)
+
+
+
+
+
+
+
+
+
+
+
