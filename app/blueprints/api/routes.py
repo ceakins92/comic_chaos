00bf894e8a753efb3855dd07a429bf8014893712
Chris Eakins <ceakins92@gmail.com>
2023-06-08 03:55:35 -0600

    API UP

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify, render_template
+# - IMPORT MODULES =======================================================
+
+from flask import Flask, request, jsonify, render_template, flash, redirect, url_for
 import requests
 import hashlib
 import pendulum
@@ -9,71 +11,78 @@ from app.models import Post, User
 from app.blueprints.api.helpers import token_required
 
 
-# - ROUTE FOR char_page
+# - ROUTE FOR char_page ===================================================
 
 @bp.route('/char_page')
 def char_page():
     return render_template('char_page.jinja')
 
-# - Character search API
+# - Character search API ==================================================
 
 def get_marvel_character(name):
     print(name)
-    # - Get Public Key from env
+    # - Get Public Key from env ==
     public_key = os.environ.get('PUBLIC_KEY')
-    # - Get Timestamp for Marvel API requirement
+    # - Get Timestamp for Marvel API requirement ========
     ts = pendulum.now('UTC')
     ts = ts.to_iso8601_string()
-    # - Generate Hash for Marvel API requirement
+    # - Generate Hash for Marvel API requirement ====
     hash = hashlib.md5()
     hash.update(ts.encode('utf-8'))
     hash.update(os.environ.get('PRIVATE_KEY').encode('utf-8'))
     hash.update(os.environ.get('PUBLIC_KEY').encode('utf-8'))
-    # - Set required Params
+    # - Set required Params ======
     params = {
         'apikey': public_key,
         'ts': ts,
         'hash': hash.hexdigest(),
         'name': name
     }
-    # - API call
+    # - API call =========
     response = requests.get(
         'http://gateway.marvel.com/v1/public/characters', params=params)
-    # - Check Response code and generate data from json response
+    # - Check Response code and generate data from json response ====
     if response.status_code == 200:
         print(response.json())
         data = response.json()
-        # - Format data to retrieve desired output
+        # - Format data to retrieve desired output ======
         try:
+            # - Create Variables for Data=========
             char_name = data["data"]["results"][0]["name"]
             description = data["data"]["results"][0]["description"]
             comics_avail = data["data"]["results"][0]["comics"]["available"]
-            comic_resource = data["data"]["results"][0]["comics"]["collectionURI"]
+            comic_resource = data["data"]["results"][0]["urls"][2]["url"]
             thumbnail = data["data"]["results"][0]["thumbnail"]["path"]
             extension = data["data"]["results"][0]["thumbnail"]["extension"]
+        # - Format Variables as Strings========
+            name_title = f'<strong>{char_name}</strong>'
+            desc_title = f'<font color="white">Description:</font> {description}'
+            comic_num = f'<font color="white">Comics Appeared In:</font> {comics_avail}'
+            comic_res = f'<font color="white">{char_name} Comic Collection via</font><font color="red"> MARVEL</font><font color="white">:</font><br/><a href="{comic_resource}" target="_blank">{comic_resource}</a>'
             thumbnail = f"{thumbnail}/landscape_incredible.{extension}"
-            return {"Character Name": char_name, "Description (if available)": description, "Comics Appeared In": comics_avail, "Comic Collection via Marvel": comic_resource,
-                  "thumbnail": thumbnail}
-        # - Error handling for output
+        # - Return Data=============
+            return {"Character Name": name_title, "Description (if available)": desc_title, "Comics Appeared In": comic_num, "Comic Collection via Marvel": comic_res,"thumbnail": thumbnail}
+    # - Error handling========
         except:
-            print('Error retrieving Character.  Please try again.')
-    # - Error handling for API Response
+            return None
+    # - Error handling for API Response====
     else:
-        print('Error, please try again')
+        flash(f"Error, Please Try Again.", "warning")
 
+# User input and error handling ========================================
    
 @bp.route('/char_page', methods=['GET','POST'])
 def char_page_post():
     if request.method == 'POST':
-        try:
-            name = request.form.get['search_char']
-            data = get_marvel_character(name)
-            if data:
-                return render_template('char_page.jinja', data=data)
-            else:
-                return "Error, Character not found."
-        except:
-            return "Error, something went wrong, pleas try again."
+        name = request.form.get('search_char')
+        if not name:
+            flash(f"Error, Please Enter a Character Name.", "warning")
+        data = get_marvel_character(name)
+        if data:
+            return render_template('char_page.jinja', data=data)
+        if data == None:
+            flash(f'Invalid Character or Character Not Found.', "warning")
+            return render_template('char_page.jinja')
     else:
         return render_template('char_page.jinja')
 
@@ -92,6 +101,7 @@ def on_sale():
         return render_template('marketplace.jinja', cover=issues["Covers"], publisher=issues["Publisher"], issue=issues["Issue"], sale_date=issues["On-sale Date"])
       
 
+# - Output user posts API ===============================================
 
 @bp.get('/posts')
 @token_required
@@ -111,6 +121,8 @@ def api_posts(user):
             })
     return jsonify(result), 200
 
+# - Posts by Username =================================================
+
 @bp.get('/posts/<username>')
 @token_required
 def user_posts(user,username):
@@ -127,6 +139,8 @@ def user_posts(user,username):
             'author':post.user_id
               } for post in user.posts]), 200
     return jsonify([{'message':'Invalid Username'}]), 404 
+
+# - Posts by ID ======================================================
 
 @bp.get('/post/<id>')
 @token_required
@@ -146,6 +160,8 @@ def get_post(user,post_id):
     except: 
       return jsonify([{'message':'Invalid Post Id'}]), 404
     
+# - Post Creation ======================================================
+
 @bp.post('/post')
 @token_required
 def make_post(user):
